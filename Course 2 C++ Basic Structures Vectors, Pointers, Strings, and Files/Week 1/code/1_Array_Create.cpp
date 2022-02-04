6a9d673ce1a5e4d3bb20b96fe28cd9f28710ecc0
Pramatma Vishwakarma <vpramatma@gmail.com>
2022-02-05 00:45:26 +0530

    C++ Basic Structures  Vectors, Pointers, Strings, and Files

---
+++ Course 2 C++ Basic Structures Vectors, Pointers, Strings, and Files/Week 1/code/1_Array_Create.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+    void withoutArray(){
+        int Allan = 71;
+        int Bob = 42;
+        int Carol = 37;
+        int David = 5;
+        int Ellen = 18;
+
+        cout << Allan << endl;
+    }
+
+int main (){
+
+    cout << "Without Arrar Creations" << endl; 
+    withoutArray();
+
+    return 0;
+}
